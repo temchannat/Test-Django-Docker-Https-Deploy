32e8bff79b8474670dbfb0dcece13ac1aa5991b1
temchannat <channat.tem@okoone.com>
2022-11-10 22:56:27 +0700

    Finish code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Test-Django-Docker-Https-Deploy
+
+# Resources:
+- Guide: https://londonappdeveloper.com/django-docker-deployment-with-https-using-letsencrypt/
+- Video: https://www.youtube.com/watch?v=3_ZJWlf25bY&t=3182s&ab_channel=LondonAppDeveloper
--- app/app/settings.py
+++ app/app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6t+2n1h)ha&06(#r7-)l46dq7r(y9y!qc0w*-fwb=)zim#10)w'
+SECRET_KEY = os.environ.get('DJANGO_SECRED_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DJANGO_DEBUG', 0)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] if DEBUG else os.environ.get('DJANGO_ALLOWED_HOSTS').split(',')
 
 
 # Application definition
